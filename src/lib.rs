c7379f3d56670a05958097bfde2bc1d1a229e338
Callum Dunster <cdunster@users.noreply.github.com>
2023-08-14 23:26:48 +0200

    docs(crate): add doc-comment to crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! This crate provides traits that match the public API of the Embassy types.
+//! These traits are implemented in this crate for the Embassy types, the implementation is a
+//! simple wrapper for the public API. This crate also provides mocked versions of these types
+//! which also implement the traits provided so they can be used to replace the real types in unit
+//! tests.
+
 #![no_std]
 #![cfg_attr(test, feature(type_alias_impl_trait))]
 #![warn(missing_docs)]
