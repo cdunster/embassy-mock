1f7291abba3f9ab77bf5ec43e74af712a9779ae5
Callum Dunster <cdunster@users.noreply.github.com>
2023-08-14 23:28:13 +0200

    docs(executor): add doc-comments with examples

--- src/executor.rs
+++ src/executor.rs
@@ -1,22 +1,111 @@
+//! A mocked version of the `embassy-executor` crate.
+//!
+//! # Examples
+//! ```
+//! # #![feature(type_alias_impl_trait)]
+//! #
+//! use embassy_mock::executor::Spawner;
+//!
+//! #[embassy_executor::task]
+//! async fn example_task() {}
+//!
+//! // Generic over the `Spawner` trait
+//! pub fn spawn_tasks<S: Spawner>(spawner: &S) {
+//!     spawner.spawn(example_task()).unwrap();
+//! }
+//!
+//! // The real main that runs on the Embassy executor.
+//! // #[embassy_executor::main]
+//! // async fn main(spawner: embassy_executor::Spawner) {
+//! //     spawn_tasks(&spawner);
+//! // }
+//!
+//! # test_spawning_of_tasks();
+//! // The unit tests that use the `MockSpawner`.
+//! #[cfg(test)]
+//! mod tests {
+//!     use super::*;
+//! # }
+//!     use embassy_mock::executor::MockSpawner;
+//!
+//!     #[test]
+//!     # fn hidden_fake_test(){}
+//!     fn test_spawning_of_tasks() {
+//!         let spawner = MockSpawner::<1>::new();
+//!         spawn_tasks(&spawner);
+//!     }
+//! # mod closing {
+//! }
+//! ```
+
 use core::sync::atomic::{AtomicUsize, Ordering};
 use embassy_executor::{SpawnError, SpawnToken, Spawner as EmbassySpawner};
 
+/// The trait to replace the [`embassy_executor::Spawner`] in code to allow the [`MockSpawner`] to
+/// be used in its place for tests.
 pub trait Spawner {
+    /// Wrapper for [`embassy_executor::Spawner::spawn()`].
     fn spawn<S>(&self, token: SpawnToken<S>) -> Result<(), SpawnError>;
 }
 
 impl Spawner for EmbassySpawner {
+    /// Spawn a task into an executor.
+    ///
+    /// You obtain the `token` by calling a task function (i.e. one marked with `#[embassy_executor::task]`).
     fn spawn<S>(&self, token: SpawnToken<S>) -> Result<(), SpawnError> {
         self.spawn(token)
     }
 }
 
+/// A mocked version of [`embassy_executor::Spawner`] that can be used in its place for unit tests.
+///
+/// This mocked version counts how many times [`Self::spawn()`] is called and asserts that it was
+/// called the correct number of times when dropped.
+///
+/// The const generic argument `N` is used to declare how many times [`Self::spawn()`] should be
+/// called.
+///
+/// # Examples
+///
+/// ```
+/// # #![feature(type_alias_impl_trait)]
+/// #
+/// use embassy_mock::executor::{MockSpawner, Spawner};
+///
+/// #[embassy_executor::task]
+/// async fn example_task() {}
+///
+/// let spawner = MockSpawner::<1>::new();  // Expects `spawn()` to be called once.
+/// spawner.spawn(example_task()).unwrap(); // `spawn()` is called once.
+/// ```
+///
+/// ```should_panic
+/// # #![feature(type_alias_impl_trait)]
+/// #
+/// use embassy_mock::executor::{MockSpawner, Spawner};
+///
+/// #[embassy_executor::task]
+/// async fn example_task() {}
+///
+/// let spawner = MockSpawner::<2>::new();  // Expects `spawn()` to be called twice.
+/// spawner.spawn(example_task()).unwrap(); // `spawn()` is called only once.
+/// ```
 #[derive(Debug)]
 pub struct MockSpawner<const N: usize> {
     times_called: AtomicUsize,
 }
 
 impl<const N: usize> MockSpawner<N> {
+    /// Create a new [`MockSpawner`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use embassy_mock::executor::MockSpawner;
+    ///
+    /// # const X: usize = 0;
+    /// let spawner = MockSpawner::<X>::new(); // Where `X` is the number of times `spawn()` should be called
+    /// ```
     pub const fn new() -> Self {
         Self {
             times_called: AtomicUsize::new(0),
