911a95133626d17efee5a8fc77e42220036c704d
Callum Dunster <cdunster@users.noreply.github.com>
2023-08-15 21:30:37 +0200

    chore(examples): add ticker example

---
+++ examples/ticker.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Spawner;
+use embassy_mock::time::Ticker;
+
+pub async fn use_a_ticker<T: Ticker>(ticker: &mut T) {
+    ticker.next().await;
+    // Do something.
+    ticker.next().await;
+    // Do something else.
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use embassy_futures::block_on;
+    use embassy_mock::time::MockTicker;
+
+    #[test]
+    fn use_mocked_ticker_type() {
+        let mut ticker = MockTicker::<2>::new();
+        block_on(ticker.next());
+        block_on(ticker.next());
+
+        ticker.done().unwrap();
+    }
+}
