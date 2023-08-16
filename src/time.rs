46aa8b1b28fe63cc955e95c4fb50db7de157528f
Callum Dunster <cdunster@users.noreply.github.com>
2023-08-16 09:21:37 +0200

    feat(time): add MockTimer with `after()` support

--- src/time.rs
+++ src/time.rs
@@ -1,5 +1,7 @@
 //! A mocked version of the `embassy-time` crate.
 
 pub mod ticker;
+pub mod timer;
 
 pub use ticker::{MockTicker, MockTickerError, Ticker};
+pub use timer::{MockTimer, Timer};
---
+++ src/time/timer.rs
@@ -0,0 +1,126 @@
+//! Traits and mocked types to allow unit testing functions that require an
+//! [`embassy_time::Timer`].
+//!
+//! # Examples
+//! ```
+//! # #![feature(type_alias_impl_trait)]
+//! #
+//! use embassy_mock::time::Timer;
+//! use embassy_time::Duration;
+//!
+//! // Generic over the `Timer` trait
+//! async fn wait_for_timer<T: Timer>() {
+//!     T::after(Duration::from_secs(1)).await;
+//!     // Do something..
+//! }
+//!
+//! // The real task that runs on the Embassy executor.
+//! #[embassy_executor::task]
+//! async fn some_task() {
+//!     wait_for_timer::<embassy_time::Timer>().await;
+//! }
+//!
+//! # test_timer_after();
+//! // The unit tests that use the `MockTimer`.
+//! #[cfg(test)]
+//! mod tests {
+//!     use super::*;
+//! # }
+//!     use embassy_futures::block_on;
+//!     use embassy_mock::time::MockTimer;
+//!
+//!     #[test]
+//!     # fn hidden_fake_test(){}
+//!     fn test_timer_after() {
+//!         block_on(wait_for_timer::<MockTimer>());
+//!     }
+//! # mod closing {
+//! }
+//! ```
+
+use core::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use embassy_time::{Duration, Timer as EmbassyTimer};
+
+/// The trait to replace the [`embassy_time::Timer`] in code to allow the [`MockTimer`] to
+/// be used in its place for tests.
+pub trait Timer: Future {
+    /// Wrapper for [`embassy_time::Timer::after()`].
+    fn after(duration: Duration) -> Self;
+}
+
+impl Timer for EmbassyTimer {
+    /// Expire after specified [`Duration`].
+    /// This can be used as a sleep abstraction.
+    ///
+    /// Example:
+    /// ``` no_run
+    /// # #![feature(type_alias_impl_trait)]
+    /// #
+    /// # fn foo() {}
+    /// use embassy_time::{Duration, Timer};
+    ///
+    /// #[embassy_executor::task]
+    /// async fn demo_sleep_seconds() {
+    ///     // suspend this task for one second.
+    ///     Timer::after(Duration::from_secs(1)).await;
+    /// }
+    /// ```
+    fn after(duration: Duration) -> Self {
+        Self::after(duration)
+    }
+}
+
+/// A mocked version of [`embassy_time::Timer`] that can be used in its place for unit tests.
+///
+/// This mocked version just immediately returns [`Poll::Ready`] when `await`'ed on.
+///
+/// # Examples
+///
+/// ```
+/// use embassy_futures::block_on;
+/// use embassy_mock::time::{MockTimer, Timer};
+/// use embassy_time::Duration;
+///
+/// let timer = MockTimer::after(Duration::from_secs(1));
+/// block_on(timer);
+/// ```
+#[derive(Debug, PartialEq, Eq)]
+pub struct MockTimer;
+
+impl Future for MockTimer {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Ready(())
+    }
+}
+
+impl Timer for MockTimer {
+    fn after(_duration: Duration) -> Self {
+        Self {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use embassy_futures::block_on;
+
+    #[test]
+    fn can_create_timer_with_after() {
+        let timer = MockTimer::after(Duration::from_secs(1));
+
+        assert_eq!(timer, MockTimer);
+    }
+
+    #[test]
+    fn can_timer_impls_future() {
+        let timer = MockTimer::after(Duration::from_secs(1));
+
+        block_on(timer);
+    }
+}
