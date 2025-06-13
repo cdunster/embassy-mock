509f56db80257eba9c350960fe921f07de3f2d2b
Callum Dunster <cdunster@users.noreply.github.com>
2025-06-13 14:56:05 +0200

    chore(nix): use git-hooks' flake-parts module

--- flake.nix
+++ flake.nix
@@ -15,7 +15,11 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
 
-    perSystem = { self', pkgs, system, ... }: {
+    imports = [
+      inputs.git-hooks.flakeModule
+    ];
+
+    perSystem = { config, pkgs, system, ... }: {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = [ inputs.rust-overlay.overlays.default ];
@@ -23,51 +27,48 @@
 
       formatter = pkgs.nixpkgs-fmt;
 
-      checks.pre-commit = inputs.git-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          # Nix
-          nixpkgs-fmt.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
+      pre-commit.settings.hooks = {
+        # Nix
+        nixpkgs-fmt.enable = true;
+        deadnix.enable = true;
+        statix.enable = true;
 
-          # Rust
-          rustfmt.enable = true;
+        # Rust
+        rustfmt.enable = true;
 
-          # Git
-          check-merge-conflicts.enable = true;
-          no-commit-to-branch.enable = true;
-          commitizen.enable = true;
+        # Git
+        check-merge-conflicts.enable = true;
+        no-commit-to-branch.enable = true;
+        commitizen.enable = true;
 
-          # TOML
-          taplo.enable = true;
+        # TOML
+        taplo.enable = true;
 
-          # Markdown
-          markdownlint = {
-            enable = true;
-            settings.configuration = {
-              # Allow duplicate headings if they have different parents (for the changelog)
-              no-duplicate-heading.siblings_only = true;
-            };
+        # Markdown
+        markdownlint = {
+          enable = true;
+          settings.configuration = {
+            # Allow duplicate headings if they have different parents (for the changelog)
+            no-duplicate-heading.siblings_only = true;
           };
-          mdformat.enable = true;
-
-          # Spell checking
-          typos.enable = true;
-
-          # Whitespace
-          mixed-line-endings.enable = true;
-          trim-trailing-whitespace.enable = true;
-
-          # Private keys
-          detect-private-keys.enable = true;
         };
+        mdformat.enable = true;
+
+        # Spell checking
+        typos.enable = true;
+
+        # Whitespace
+        mixed-line-endings.enable = true;
+        trim-trailing-whitespace.enable = true;
+
+        # Private keys
+        detect-private-keys.enable = true;
       };
 
       devShells.default = with pkgs; mkShell {
         packages = [
           (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-          self'.checks.pre-commit.enabledPackages
+          config.pre-commit.settings.enabledPackages
           git-cliff
           cargo-make
           cargo-llvm-cov
@@ -75,7 +76,7 @@
 
         RUST_SRC_PATH = rustPlatform.rustLibSrc;
 
-        inherit (self'.checks.pre-commit) shellHook;
+        shellHook = "${config.pre-commit.installationScript}";
       };
     };
   };
