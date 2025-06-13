fe0ad574abe8d9e741bd2e0dc5eee04a7c12372d
Callum Dunster <cdunster@users.noreply.github.com>
2025-06-13 13:21:24 +0200

    style(nix): move statements onto single lines where possible

--- flake.nix
+++ flake.nix
@@ -12,45 +12,42 @@
     };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" ];
 
-      perSystem = { self', pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
-        };
+    perSystem = { self', pkgs, system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ inputs.rust-overlay.overlays.default ];
+      };
 
-        formatter = pkgs.nixpkgs-fmt;
+      formatter = pkgs.nixpkgs-fmt;
 
-        checks = {
-          pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              rustfmt.enable = true;
-              commitizen.enable = true;
-              taplo.enable = true;
-            };
-          };
-        };
-
-        devShells.default = with pkgs; mkShell {
-          packages = [
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-            self'.checks.pre-commit.enabledPackages
-            git-cliff
-            cargo-make
-            cargo-tarpaulin
-            cargo-toml-lint
-            cargo-llvm-cov
-          ];
-
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-
-          inherit (self'.checks.pre-commit) shellHook;
+      checks.pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixpkgs-fmt.enable = true;
+          rustfmt.enable = true;
+          commitizen.enable = true;
+          taplo.enable = true;
         };
       };
+
+      devShells.default = with pkgs; mkShell {
+        packages = [
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          self'.checks.pre-commit.enabledPackages
+          git-cliff
+          cargo-make
+          cargo-tarpaulin
+          cargo-toml-lint
+          cargo-llvm-cov
+        ];
+
+        RUST_SRC_PATH = rustPlatform.rustLibSrc;
+
+        inherit (self'.checks.pre-commit) shellHook;
+      };
     };
+  };
 }
